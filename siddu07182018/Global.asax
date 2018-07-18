0e50d551af23fd4d57fe68a48eda260c0b4d6533
siddu <siddugulbarga011@gmail.com>
2018-07-18 10:39:27 +0530

    siddu

---
+++ siddu07182018/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="siddu07182018.MvcApplication" Language="C#" %>
---
+++ siddu07182018/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(siddu07182018.Startup))]
+namespace siddu07182018
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
